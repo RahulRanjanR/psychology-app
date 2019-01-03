30373bb4cb7636b4a9f4ab62df422e485586885f
maoziyou <kobetbuckley@gmail.com>
2019-01-02 21:19:28 -0500

    testing reducers

---
+++ src/reducers.test.js
@@ -0,0 +1,78 @@
+import {
+  CHANGE_SEARCHFIELD,
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS,
+  REQUEST_ROBOTS_FAILED
+ } from './constants';
+
+
+import * as reducers from './reducers';
+
+
+describe('searchRobots', () => {
+  const initialStateSearch = {
+    searchField: ''
+  }
+  it('should return the initial state', () => {
+    expect(reducers.searchRobots(undefined, {})).toEqual({ searchField: ''})
+  })
+
+it('should return CHANGE_SEARCHFIELD', () => {
+  expect(reducers.searchRobots(initialStateSearch, {
+    type: CHANGE_SEARCHFIELD,
+    payload: 'abc'
+  })).toEqual({
+    searchField: 'abc'
+  })
+ })
+})
+
+describe('requestRobots', () => {
+  const initialStateRobots = {
+    robots: [],
+    isPending: false
+  }
+  it('should return the initial state', () => {
+    expect(reducers.requestRobots(undefined, {})).toEqual(initialStateRobots)
+  })
+
+  it('should handle REQUEST_ROBOTS_PENDING actions', () => {
+    expect(reducers.requestRobots(initialStateRobots, {
+        type: REQUEST_ROBOTS_PENDING,
+    })).toEqual({
+      robots: [],
+      isPending: true
+    })
+  })
+
+
+it('should handle REQUEST_ROBOTS_SUCCESS action', () => {
+  expect(reducers.requestRobots(initialStateRobots, {
+      type: REQUEST_ROBOTS_SUCCESS,
+      payload: [{
+        id: '123',
+        name: 'test',
+        email: 'test@gmail.com'
+      }]
+  })).toEqual({
+    robots: [{
+        id: '123',
+        name: 'test',
+        email: 'test@gmail.com'
+      }],
+    isPending: false
+  })
+ })
+
+
+ it('should handle REQUEST_ROBOTS_FAILED action', () => {
+   expect(reducers.requestRobots(initialStateRobots, {
+       type: REQUEST_ROBOTS_FAILED,
+       payload: 'NOOOOOOO!!!'
+   })).toEqual({
+     error: 'NOOOOOOO!!!',
+     robots: [],
+     isPending: false
+   })
+  })
+})
