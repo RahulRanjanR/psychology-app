b998e86e43dd1c22ee029f92425b0159d0cec2ba
maoziyou <kobetbuckley@gmail.com>
2019-01-08 05:48:24 -0500

    adding the api

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 // *****************smart-brain features*****************
+import Clarifai from 'clarifai';
 import Particles from 'react-particles-js';
+import FaceRecognition from '../components/FaceRecognition/FaceRecognition';
 import Navigation from '../components/Navigation/Navigation';
 import Logo from '../components/Logo/Logo';
 import ImageLinkForm from '../components/ImageLinkForm/ImageLinkForm';
@@ -10,6 +12,10 @@ import { connect } from 'react-redux';
 import { setSearchField, requestRobots } from '../actions';
 import MainPage from '../components/MainPage';
 import './App.css';
+
+const app = new Clarifai.App({
+ apiKey: '9a8ca46ac9bf443a9d35f6de69d313f0'
+});
 
 const mapStateToProps = (state) => {
   return {
@@ -26,27 +32,36 @@ const mapDispatchToProps = (dispatch) => {
   }
 }
 
-// const particlesOptions = {
-//     particles: {
-//       number: {
-//         value: 90,
-//         density: {
-//           emable: true,
-//           value_area: 800
-//         }
-//       }
-//     }
-//   }
-
-
-// insert this into the return if wanted
-// <Particles className='particles'
-//     params={particlesOptions}
-//   />
-
 
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+    }
+  }
+
+  onInputChange = (event) => {
+      this.setState({input: event.target.value});
+  }
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input})
+    app.models.predict(
+      Clarifai.FACE_DETECT_MODEL,
+      this.state.input)
+      .then(
+      function(response) {
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+        // do something with response
+      },
+      function(err) {
+        // there was an error
+      }
+    );
+  }
+
   render() {
     return (
       <div className="App">
@@ -54,10 +69,11 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-  {/*
-        <FaceRecognition />
-        */}
+        <ImageLinkForm
+         onInputChange={this.onInputChange}
+         onButtonSumbit={this.onButtonSubmit}
+         />
+        <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     );
   }
@@ -65,5 +81,33 @@ class App extends Component {
 
 export default connect(mapStateToProps, mapDispatchToProps)(App)
 
+//******************************************************************//
+
 // goes in the render() for route page
     // return <MainPage { ...this.props } />
+
+//******************************************************************//
+
+
+
+
+
+
+
+    // const particlesOptions = {
+    //     particles: {
+    //       number: {
+    //         value: 90,
+    //         density: {
+    //           emable: true,
+    //           value_area: 800
+    //         }
+    //       }
+    //     }
+    //   }
+
+
+    // insert this into the return if wanted
+    // <Particles className='particles'
+    //     params={particlesOptions}
+    //   />
