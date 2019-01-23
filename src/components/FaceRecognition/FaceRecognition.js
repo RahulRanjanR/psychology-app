9c7ec449b7532b78f878c87ee1815ba2930f954a
maoziyou <kobetbuckley@gmail.com>
2019-01-23 11:36:29 -0500

    removed the face replacement feature

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './FaceRecognition.css'
-import psychology from '../Logo/psychology.png';
-import hmm from './hmm.png'
+
 
 const FaceRecognition = ({imageUrl, boxes}) => {
   return  (
     <div className='center ma'>
     <div className='absolute mt2'>
-      <img id='inputImage' alt='' src={imageUrl} width='600px' height= 'auto'/>
+      <img id='inputImage' alt='' src={imageUrl} width='400px' height= 'auto'/>
     {boxes.map(box => {
-    return  <img alt='' src={hmm}  key ={box.topRow}className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}/>
+      return  <div  key ={box.topRow}className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
     })
     }
+
 
       </div>
     </div>
