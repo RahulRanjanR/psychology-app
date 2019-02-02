1e7fb600fdc15080a0f5847560871bc886da2845
kobebuckley <kobetbuckley@gmail.com>
2019-02-02 13:42:49 -0500

    small mistake

--- src/UserSearchPageExtras/MainPage.test.js
+++ src/UserSearchPageExtras/MainPage.test.js
@@ -1,6 +1,6 @@
 import { shallow } from 'enzyme';
 import React from 'react';
-import MainPage from '../components/UserSearchPage/Card/MainPage'
+import MainPage from '../components/UserSearchPage/MainPage'
 
 let wrapper;
 beforeEach(() => {
